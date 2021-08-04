98176b51be8a600a0bf98b4e642f2b707341a8cd
Sergey-Rogalskiy <rogalskiy.sergey@gmail.com>
2021-08-04 11:23:14 +0300

    feat: restAPI for rooms

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const userRoute = require('./routes/rooms')
 
 const app = express()
-
 const port = 3001
+
+app.use(express.json())
+app.use('/api/rooms/', userRoute)
 
 app.listen(port, () => {
     console.log(`Server has been started on port ${port}`)
